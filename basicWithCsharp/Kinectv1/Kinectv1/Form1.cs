9f5d537bec902d5d8420df1cac995bfbdd9e04a4
IZERO_DESKTOP <izero3127@gmail.com>
2021-09-06 01:59:47 +0900

    [kinect] C#으로 kinect camera 크고끄기

---
+++ basicWithCsharp/Kinectv1/Kinectv1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure.Kinect.Sensor;
+
+namespace Kinectv1
+{
+    public partial class Form1 : Form
+    {
+        //키넥트 변수
+        Device Kinect;
+        public Form1()
+        {
+            InitializeComponent();
+            initKinect();
+        }
+
+        private void initKinect()
+        {
+            Kinect = Device.Open(0);
+
+            //키넥트 모드 설정
+            Kinect.StartCameras(new
+                DeviceConfiguration
+            {
+                ColorFormat = ImageFormat.ColorBGRA32,
+                ColorResolution = ColorResolution.R720p,
+                DepthMode = DepthMode.NFOV_2x2Binned,
+                CameraFPS = FPS.FPS30
+            });
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Kinect.StopCameras();
+        }
+    }
+}
