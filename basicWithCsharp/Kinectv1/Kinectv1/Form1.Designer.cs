8598223031679262c16fd8b61e5e5fc06f8bbd34
IZERO_DESKTOP <izero3127@gmail.com>
2021-09-08 04:03:06 +0900

    [Kinect] C : C# 자꾸 폼 안떠서 그냥 C로 옮겨탐

--- basicWithCsharp/Kinectv1/Kinectv1/Form1.Designer.cs
+++ basicWithCsharp/Kinectv1/Kinectv1/Form1.Designer.cs
@@ -29,21 +29,36 @@ namespace Kinectv1
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(78, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- basicWithCsharp/Kinectv1/Kinectv1/Form1.cs
+++ basicWithCsharp/Kinectv1/Kinectv1/Form1.cs
@@ -9,19 +9,82 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Azure.Kinect.Sensor;
 
+// 키넥트에서 취득한 이미지의 포인터 접근용
+using System.Buffers;
+
+// AzureKinect와 System 변수 이름의 모호성을 없애는 용
+using Image = Microsoft.Azure.Kinect.Sensor.Image;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using System.Diagnostics;
 namespace Kinectv1
 {
     public partial class Form1 : Form
     {
-        //키넥트 변수
+        // 키넥트 변수
         Device Kinect;
+        // 컬러 이미지 Bitmap
+        Bitmap colorBitmap;
+        /*키넥트 이미지 획득 여부
+        당연히 이미지는 계속 얻어야 하니까 그거 반복위한 변수*/
+        bool loop = true;
+
         public Form1()
         {
             InitializeComponent();
-            initKinect();
+            InitKinect();
+
+            //키넥트 설정 정보에 따른 Bitmap 관련 정보 초기화
+            InitBitmap();
+            Task t = KinectLoop();
         }
 
-        private void initKinect()
+        //비동기식 async로 데이터획득 할 것
+        private async Task KinectLoop()
+        {
+            while(loop)
+            {
+                //Kinect에서 새로운 데이터를 받음
+                using (Capture capture = await Task.Run(() =>
+                Kinect.GetCapture()).ConfigureAwait(true))
+                { //안전하지 않은 코드 사용
+                    unsafe
+                        {
+                        //컬러 이미지를 얻음
+                        Image colorImage = capture.Color;
+                        //이미지의 메모리 주소를 얻음
+                        using(MemoryHandle pin = colorImage.Memory.Pin()) 
+                        {
+                            //Bitmap 이미지 생성
+                            colorBitmap = new Bitmap(
+                                colorImage.WidthPixels,
+                                colorImage.HeightPixels,
+                                colorImage.StrideBytes,
+                                PixelFormat.Format32bppArgb,
+                                (IntPtr)pin.Pointer);
+                        }
+                    }
+                    //pictureBox에 생성한 이미지를 붙여넣음
+                    pictureBox1.Image = colorBitmap;
+                }
+                //이미지 표시를 업데이트
+                this.Update();
+            }
+            //루프가 끝나면 Kinect도 정지
+            Kinect.StopCameras();
+        }
+
+        // Bitmap 이미지에 대한 초기 설정
+        private void InitBitmap()
+        {
+            // 컬러 이미지의 가로 폭과 세로 폭 취득
+            int width = Kinect.GetCalibration().ColorCameraCalibration.ResolutionWidth;
+            int height = Kinect.GetCalibration().ColorCameraCalibration.ResolutionHeight;
+
+            // Bitmap 이미지 생성
+            colorBitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        }
+
+        private void InitKinect()
         {
             Kinect = Device.Open(0);
 
@@ -38,6 +101,8 @@ namespace Kinectv1
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // 키넥트에서 반복 멈추도록 하면 카메라 멈춤
+            loop = false;
             Kinect.StopCameras();
         }
     }
