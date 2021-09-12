9a7370b534766e8e7028c764e50822c583ae8444
IZERO_DESKTOP <izero3127@gmail.com>
2021-09-13 02:51:00 +0900

    아까와 일치

---
+++ basicWithC/kinectv2/kinectv2/k4aCmd.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//************************ Includes *****************************
+
+//**************Symbolic Constant Macros (defines)  *************
+
+//************************ Typedefs *****************************
+
+//************ Declarations (Statics and globals) ***************
+
+//******************* Function Prototypes ***********************
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	void k4a_cmd_init(void);
+
+#ifdef __cplusplus
+}
+#endif
