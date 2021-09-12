768dbae080997a44f473ac68c7761cca597c9e1a
IZERO_DESKTOP <izero3127@gmail.com>
2021-09-13 02:50:05 +0900

    ckinectv1 잘되는중 : nuget 확인해볼것 항상

--- basicWithCsharp/Kinectv1/Kinectv1/Form1.Designer.cs
+++ basicWithCsharp/Kinectv1/Kinectv1/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace Kinectv1
+namespace Csharp_3d_viewer
 {
     partial class Form1
     {
@@ -12,14 +12,6 @@ namespace Kinectv1
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
         /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
 
         #region Windows Form 디자이너에서 생성한 코드
 
